a4a3374771ef86342387a47137e2d4d2fb41b692
pervezmia <ahmedpervezkabir@gmail.com>
2026-05-02 12:13:19 +0600

    google sing in and signout added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,17 @@
 "use client";
-import { Button } from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+import { Avatar, Button } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
 
 const Navbar = () => {
+  const userData = authClient.useSession();
+  // const user = userData.data.user;
+  const user = userData.data?.user;
+  console.log(user);
+  const handleSignOut = async () => {
+    await authClient.signOut();
+  }
   return (
     <div className="border-b px-2 mb-4">
       <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
@@ -26,18 +34,37 @@ const Navbar = () => {
           <li>
             <Link href={"/all-animals"}>All Animals</Link>
           </li>
-          
         </ul>
 
         <div className="flex gap-4">
-          <ul className="flex items-center gap-2 text-sm">
-            <li>
-              <Link href={"/register"}><Button variant="outline">Register</Button></Link>
-            </li>
-            <li>
-              <Link href={"/signin"}><Button>Log In</Button></Link>
-            </li>
-          </ul>
+          {!user && (
+            <ul className="flex items-center gap-2 text-sm">
+              <li>
+                <Link href={"/register"}>
+                  <Button variant="outline">Register</Button>
+                </Link>
+              </li>
+              <li>
+                <Link href={"/signin"}>
+                  <Button>Log In</Button>
+                </Link>
+              </li>
+            </ul>
+          )}
+
+          {user && (
+            <div className="flex gap-3.5">
+              <Avatar>
+                <Avatar.Image
+                  alt={user?.name}
+                  src={user?.image}
+                  referrerPolicy="no-referrer"
+                />
+                <Avatar.Fallback>{user?.name.charAt(0)}</Avatar.Fallback>
+              </Avatar>
+              <Button onClick={handleSignOut} variant="danger">Sign Out</Button>
+            </div>
+          )}
         </div>
       </nav>
     </div>
