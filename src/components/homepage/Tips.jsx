6f158595ada67f8bf4cf25a8259080d50c744471
pervezmia <ahmedpervezkabir@gmail.com>
2026-05-02 19:05:08 +0600

    category json file added

--- src/components/homepage/Tips.jsx
+++ src/components/homepage/Tips.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import { Card } from "@heroui/react";
+import React from "react";
 
 const Tips = async () => {
-    const res = await fetch ("https://a-8-qurbani-hat.vercel.app/benefits.json");
-    // const data = await res.json();
-    // console.log(data);
-    console.log(res);
-    return (
-        <div>
-            
+  const res = await fetch("https://a-8-qurbani-hat.vercel.app/benefits.json");
+  const data = await res.json();
+  console.log(data);
+  // console.log(res);
+  return (
+    <div>
+        <h3 className="font-bold mt-10 mb-3">Tips and Tricks</h3>
+        <div className="flex gap-3">
+      {data.map((d) => (
+        <div key={d.id}>
+          <Card>
+            <div className="space-y-4">
+              <h2 className="font-black">{d.name} {d.title}</h2>
+              
+                <ul className="list-none">
+                    {
+                    d.benefits.map((b, inx )=> <li className="" key={inx}><p className="mt-3 p-2 rounded-xl bg-green-200" >{b}</p></li>)
+                }
+                </ul>
+              
+              <p>{d.description}</p>
+            </div>
+          </Card>
         </div>
-    );
+      ))}
+    </div>
+    </div>
+  );
 };
 
 export default Tips;
